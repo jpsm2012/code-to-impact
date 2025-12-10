967c307706a709a02e57fac97dbaf47ec55fbe20
jpsm2012 <joaopedro@papx.com.br>
2025-12-09 22:00:54 -0300

    Create simulador_de_dinheiro.py

---
+++ simulador_de_dinheiro.py
@@ -0,0 +1,6 @@
+print("bem vindo a simulaçao")
+din= float(input("coloque o dinheiro :"))
+dias= int(input("coloque quantos dias sera a simulaçao:"))
+
+calculo=(din*(2)**(dias-1))
+print("voce vai ter,",calculo,",dinheiros")

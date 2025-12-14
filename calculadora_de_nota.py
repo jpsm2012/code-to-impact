ae0c443225c3a10d152c7e7b512c50f15cb884d6
jpsm2012 <joaopedro@papx.com.br>
2025-12-13 22:30:00 -0300

    Create calculadora_de_nota.py
    muito útil !!!

---
+++ calculadora_de_nota.py
@@ -0,0 +1,15 @@
+print("vamos calcular a sua media:")
+
+b1 = float(input("Nota do 1º bimestre: ").replace(",", "."))
+b2 = float(input("Nota do 2º bimestre: ").replace(",", "."))
+b3 = float(input("Nota do 3º bimestre: ").replace(",", "."))
+b4 = float(input("Nota do 4º bimestre: ").replace(",", "."))
+
+media = (b1 + b2 + b3 + b4) / 4
+
+print(f"Sua média final é: {media}")
+
+if media >= 7:
+    print("Parabéns! Você foi aprovado!")
+else:
+    print("nao foi dessa vez !!!")

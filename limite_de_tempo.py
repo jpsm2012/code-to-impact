7f0db7a9a8e32b9545bb3483f6ce12059938e848
jpsm2012 <joaopedro@papx.com.br>
2025-12-16 21:08:44 -0300

    limite_de_tempo.py

---
+++ limite_de_tempo.py
@@ -0,0 +1,11 @@
+commits = int(input("Quantos commits você colocou no git hub "))
+ 
+                          # quantidade de commits
+if commits < 2:
+    print("ta faltando commits. Nada de xadrez 😫!!!")
+
+              #se nao atingir a aquantidade de commits,nada de xadrez
+else:
+    horas = commits // 2
+    print(f"o tempo de jogatina no xadrez e de {horas} hora(s)🥹")
+                   # xadrez liberado

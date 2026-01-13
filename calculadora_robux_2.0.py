470526d6a01e41195aaf4b7c33faa3042b24fee2
jpsm2012 <joaopedro@papx.com.br>
2026-01-13 16:56:46 -0300

    Add basic Robux calculator functionality

---
+++ calculadora_robux_2.0.py
@@ -0,0 +1,13 @@
+
+robux = int(input("Quantos Robux você possui atualmente? "))
+
+
+print("Você informou que tem", robux, "Robux.")
+
+valor = int(input("Qual é o valor do item que você quer comprar? "))
+
+print("O item custa", valor, "Robux.")
+
+quanto_falta = valor - robux
+
+print("Faltam", quanto_falta, "Robux para você conseguir comprar.")
